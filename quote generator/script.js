c96148dc89c5c996631312556e0b1fb918db02d7
aditya3131 <singhaditya3116@gmail.com>
2022-11-12 23:50:58 +0530

    API is used in UI

--- quote generator/script.js
+++ quote generator/script.js
@@ -1,1 +1,24 @@
-console.log("HEllo");
+
+// Get Quotes From API.
+let apiQuotes = [];
+const btn = document.getElementById("new-quote");
+const quote = document.getElementById("quote");
+const author = document.getElementById("author");
+
+async function getQuotes()
+{
+  const apiURL = "https://type.fit/api/quotes";
+
+  const quotesArray = await fetch(apiURL);
+  apiQuotes = await quotesArray.json();
+  console.log(apiQuotes);
+}
+
+btn.addEventListener("click",function(){
+  let randomNumber = Math.floor(Math.random()*apiQuotes.length);
+  quote.innerHTML = apiQuotes[randomNumber].text;
+  author.innerHTML = apiQuotes[randomNumber].author;
+})
+
+
+getQuotes();
