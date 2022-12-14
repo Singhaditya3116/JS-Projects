97222c537e0bf19c8265e4b6ed1206ab6e5b6c94
aditya3131 <singhaditya3116@gmail.com>
2022-12-14 21:37:15 +0530

    Form values Fetched in JS

--- Custom Countdown/script.js
+++ Custom Countdown/script.js
@@ -1,3 +1,32 @@
-// const video = document.getElementsByClassName("video-background");
-// console.log(video[0]);
-// video[0].play();
+const inputContainer = document.getElementById("input-container");
+const countdownTimerContainer = document.getElementById("countdown");
+const countdownCompleteContainer = document.getElementById("countdown-complete");
+
+const countdownForm = document.getElementById("countdown-form");
+const dateEl = document.getElementById("date");
+const countdownTitleEl = document.getElementById("countdown-title");
+
+let countdownTitle = "";
+let countdownDate = "";
+
+
+//Set date input minimum as today date.
+const today = new Date().toISOString().split("T")[0];
+dateEl.setAttribute("min",today);
+dateEl.value=today;
+
+//take form Input
+function updateCountdown(e)
+{
+  countdownTitle = e.srcElement[0].value;
+  countdownDate = e.srcElement[1].value;
+  console.log(countdownTitle,countdownDate);
+  e.preventDefault();
+
+  inputContainer.hidden = true;
+  countdownTimerContainer.hidden=false;
+  countdownTitleEl.innerText = countdownDate;
+}
+
+//Event Listeners
+countdownForm.addEventListener("submit",updateCountdown);
